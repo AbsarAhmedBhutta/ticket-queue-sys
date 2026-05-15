cf0c1a06b8f8cc807443ed4f4d373309406cf571
AbsarAhmedBhutta <absarbhutta@gmail.com>
2026-05-15 21:06:45 +0500

    tests fix

--- mainproject/tests/tickets_websocket/test_connection.py
+++ mainproject/tests/tickets_websocket/test_connection.py
@@ -3,13 +3,13 @@ from channels.testing import WebsocketCommunicator
 from mainproject.asgi import application
 
 @pytest.mark.asyncio
-async def web_socket_check_connection():
+async def test_web_socket_check_connection():
     communicator = WebsocketCommunicator(
         application,
         "ws/tickets/"
     )
     
-    connected, subprotocol = await communicator.connect()
+    connected, _ = await communicator.connect()
     assert connected is True
 
     await communicator.disconnect()
